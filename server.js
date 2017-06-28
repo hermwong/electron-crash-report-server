f76318259caa86269aadce5325f5d3d34fce66af
Herm Wong <herm.wong@gmail.com>
2017-06-28 13:08:52 -0700

    code clean up

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 const Basic = require('hapi-auth-basic')
 const Boom = require('boom')
-//const Handlebars = require('handlebars')
 const Hapi = require('hapi')
 const Vision = require('vision')
-//const db = require('./db.js')
 const analyticsRequest = require('request')
 
 const port = process.env.PORT
@@ -31,105 +29,23 @@ server.register([Basic, Vision], err => {
       }
     }
   })
-/*
-  server.views({
-    engines: {html: Handlebars},
-    relativeTo: __dirname,
-    layout: true,
-    path: 'views'
-  })
-*/
+
   server.route({
     method: 'GET',
     path: '/',
     config: {
       auth: 'simple',
       handler: (request, reply) => {
-        
-        /*
-        const sql = 'SELECT * FROM reports ORDER BY created_at DESC'
-        db.run(sql, (err, reports) => {
-          if (err) throw err
-          const auth = new Buffer(`${process.env.AUTH_USER}:${process.env.AUTH_PASS}`).toString('base64')
-          const opts = {
-            isHttpOnly: false,
-            isSecure: process.env.NODE_ENV === 'production'
-          }
-          */
-    
           reply('hello world');
-            //.view('index', {reports, title: 'crash reports'})
-            //.state('authorization', auth, opts)
-        //})
       }
     }
   })
-
-
-
-  /*
-  
-  server.route({
-    method: 'GET',
-    path: '/reports/{id}',
-    config: {
-      auth: 'simple',
-      handler: (request, reply) => {
-        const id = Number(request.params.id)
-        db.reports.find(id, (err, report) => {
-          if (err) throw err
-          report.body = JSON.stringify(report.body, null, 2)
-          reply.view('report', {report, title: 'crash report'})
-        })
-      }
-    }
-  })
-
-  server.route({
-    method: 'DELETE',
-    path: '/reports/{id}',
-    config: {
-      auth: 'simple',
-      handler: (request, reply) => {
-        const id = Number(request.params.id)
-        db.dumps.destroy({report_id: id}, (err, res) => {
-          if (err) throw err
-          db.reports.destroy({id}, (err, res) => {
-            if (err) throw err
-            reply().location('/')
-          })
-        })
-      }
-    }
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/reports/{id}/dump',
-    config: {
-      auth: 'simple',
-      handler: (request, reply) => {
-        const id = Number(request.params.id)
-        db.dumps.findOne({report_id: id}, (err, dump) => {
-          if (err) throw err
-          const filename = `crash-${id}.dmp`
-          reply(dump.file)
-            .header('content-disposition', `attachment; filename=${filename}`)
-            .type('application/x-dmp')
-        })
-      }
-    }
-  })
-  
-  */
 
   server.route({
     method: 'POST',
     path: '/',
     handler: (request, reply) => {
       if (request.payload && request.payload.prod === 'Electron') {
-
-        //var analyticsRequest = require('request');
 
         var analyticsJSON = {};
         analyticsJSON.version = request.payload.version;
@@ -148,20 +64,8 @@ server.register([Basic, Vision], err => {
         analyticsJSON.ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
 
         const payload = Object.assign({}, analyticsJSON)
-        //const dumpFilePayload = Object.assign({}, request.payload)
-        //const file = dumpFilePayload.upload_file_minidump
 
-        //delete dumpFilePayload.upload_file_minidump
-
-        //db.reports.saveDoc(payload, (err, report) => {
-        //  if (err) throw err
-
-          //db.dumps.insert({file, report_id: report.id}, (err, dump) => {
-            //if (err) throw err
-
-            reply()
-          //})
-        //})
+        reply()
 
         analyticsRequest.post({
             url: 'https://metrics.phonegap.com/gelfproxypass',
